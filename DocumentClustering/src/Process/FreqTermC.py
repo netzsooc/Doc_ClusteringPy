b4ed1a48a6bc7db99d81bc3d772ae6dae57e23b8
netzsooc <netzsooc@gmail.com>
2013-06-06 17:07:21 -0500

    No changes

--- DocumentClustering/src/Process/FreqTermC.py
+++ DocumentClustering/src/Process/FreqTermC.py
@@ -8,56 +8,60 @@ class FreqTermCluster(object):
     '''
     classdocs
     '''
-
-
-    def __init__(self,stopwords, ignorechars, docs):
+ 
+    _dcount = 0
+    vocabulary =  set()
+    docs = {}
+    freq_terms = set()
+    
+    def __init__(self,stopwords, ignorechars, docs, minsup=.3):
         '''
         Constructor
         '''
-        self._minsupp = .3
-        self._dcount = 0
+        self._minsup = minsup
         self._stopwords =  stopwords
         self._ignorechars = ignorechars
-        self.vocabulary =  set()
+#         self.
         self._get_vocabulary(docs)
-        self.docs = {}
+#         self.docs = {}
         self._set_docs(docs)
-        self.freq_terms = set()
+#         self.freq_terms = set()
         self._set_freq_terms()
-
-
+ 
+ 
     def _get_terms(self, doc):
         return set([term.lower().
                     translate(term.maketrans('','',self._ignorechars))
                     for term in doc.split() 
                       if term.lower() not in self._stopwords])
-
-
+ 
+ 
     def _get_vocabulary(self, docs):
         for doc in docs:
             terms_in_doc = self._get_terms(doc)
             self.vocabulary = self.vocabulary.union(terms_in_doc)
-
-
+ 
+ 
     def _set_docs(self, docs):
         for doc in docs:
             self.docs["D" + str(self._dcount)] = self. _get_terms(doc)
             self._dcount += 1 
-
-
+ 
+ 
     def cov(self, termset):
         termset = set([termset])
         return set([d for d in self.docs 
                     if termset.issubset(self.docs[d])])
-
-
+ 
+ 
     def _set_freq_terms(self):
         f_terms = set([term for term in self.vocabulary if 
-                        len(self.cov(term)) >= self._minsupp * len(self.docs)])
+                        len(self.cov(term)) >= self._minsup * len(self.docs)])
         self.freq_terms = self.freq_terms.union(f_terms)
-            
+
 
 def main(*args):
+    
     docs = [
             "Human machine interface for ABC computer applications",
             "A survey of user opinion of computer system response time",
@@ -69,14 +73,15 @@ def main(*args):
             "Graph minors IV: Widths of trees and well-quasi-ordering",
             "Graph minors: A survey"
             ]
+#     docs = [title for title in open("/home/netzsooc/Documents/infotec/Investigacion/justTitles.txt")]
     docstopwords = ["the", "for", "of", "a", "and", "to", "in", "an"]
     ignorechars = ",:"
     test = FreqTermCluster(docstopwords,ignorechars, docs)
+    test._minsupp = .3
 #     for doc in docs:
 #         test.get_vocabulary(doc)
-
+    print(test._minsupp)
     print(test.vocabulary)
-    print(test.docs["D0"])
     print(test.freq_terms)
     for term in test.freq_terms:
         print(test.cov(term))
