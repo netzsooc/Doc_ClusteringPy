34fb19834f013a77405f6cbbebcd3c0a68ead537
netzsooc <netzsooc@gmail.com>
2013-06-06 17:07:43 -0500

    First commit

---
+++ DocumentClustering/src/Process/prepro.py
@@ -0,0 +1,43 @@
+'''
+Created on Jun 6, 2013
+
+@author: netzsooc
+'''
+from porterstemmer import Stemmer
+
+stop = ["the", "for", "of", "a", "and", "to", "in", "an"]
+ignore = "(),:'\""
+
+
+def get_terms(doc, index, ignorechars=ignore, stopwords=stop):
+    if index:
+        return set([index[Stemmer()(term.lower().
+                translate(term.maketrans('','',ignorechars)))]
+                for term in doc.split() 
+                    if term.lower() not in stopwords])
+    return set([Stemmer()(term.lower().
+                translate(term.maketrans('','',ignorechars)))
+                for term in doc.split() 
+                    if term.lower() not in stopwords])
+
+
+def index_words(termset):
+    my_map = {}
+    i = 0
+    for term in termset:
+        my_map[term] = i
+        i += 1
+    return my_map
+
+
+def get_vocabulary(docs, ignorechars=ignore, stopwords=stop):
+    vocab = set()
+    for doc in docs:
+        terms_in_doc = get_terms(doc, False, ignorechars, stopwords)
+        vocab = vocab.union(terms_in_doc)
+    return vocab
+
+
+def cov(term_set, docs):
+    return set([d for d in docs.keys()
+                if term_set.issubset(docs[d])])
