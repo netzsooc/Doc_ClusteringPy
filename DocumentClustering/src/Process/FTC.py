962a45182d40655cdc7f2c9eb6410e8840ecb6b5
netzsooc <netzsooc@gmail.com>
2013-06-06 17:06:52 -0500

    No class FTC

--- DocumentClustering/src/Process/FTC.py
+++ DocumentClustering/src/Process/FTC.py
@@ -4,49 +4,36 @@ Created on Jun 6, 2013
 @author: netzsooc
 '''
 
-class FTC(object):
-    '''
-    classdocs
-    '''
-    
-    
-    def __init__(self, docs, minsup):
-        '''
-        Constructor
-        '''
-        self.selected_term_sets = {}
-        self.remaining_term_sets = self.get_freq_termsets(docs, minsup)
-        doc_card = len(docs)
+from prepro import get_vocabulary, get_terms, cov, index_words
 
-
-    def get_freq_termsets(self, docs, minsup):
-        pass
 
 
 def main(*args):
     
-    minsup = .3
-    docs = [
-            "Human machine interface for ABC computer applications",
-            "A survey of user opinion of computer system response time",
-            "The EPS user interface management system",
-            "System an human system engineering testing for EPS",
-            "Relation of user perceived response time to error measurement",
-            "The generation of random, binary, ordered trees",
-            "The intersection graph of paths in trees",
-            "Graph minors IV: Widths of trees and well-quasi-ordering",
-            "Graph minors: A survey"
-            ]
-#     docstopwords = ["the", "for", "of", "a", "and", "to", "in", "an"]
-#     ignorechars = ",:"
-    test = FTC(docs, minsup)
-#     for doc in docs:
-#         test.get_vocabulary(doc)
+    minsup = .05
+#     docs = [
+#             "Human machine interface for ABC computer applications",
+#             "A survey of user opinion of computer system response time",
+#             "The EPS user interface management system",
+#             "System an human system engineering testing for EPS",
+#             "Relation of user perceived response time to error measurement",
+#             "The generation of random, binary, ordered trees",
+#             "The intersection graph of paths in trees",
+#             "Graph minors IV: Widths of trees and well-quasi-ordering",
+#             "Graph minors: A survey"
+#             ]
+    docs = [title for title in open("/home/netzsooc/Documents/infotec/Investigacion/cleanAbstracts.txt")]
+    
 
-    print(test.vocabulary)
-    print(test.docs["D0"])
-    print(test.freq_terms)
-    for term in test.freq_terms:
-        print(test.cov(term))
+    T = get_vocabulary(docs)
+    enc = index_words(T)
+    dec = dict([(v,k) for k,v in enc.items()])
+    D = {}
+    for i in range(len(docs)):
+        D["D" + str(i)] = get_terms(docs[i], enc)
+    print(len(T))
+    F = set([enc[i] for i in T if len(cov({enc[i]}, D)) >= minsup * len(D)])
+    print(len(F))
 
+    
 if __name__ == "__main__": main()
